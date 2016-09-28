e11e17cbd116b15fcb3c6af5a470239f0e7661f5
Alaa El Helw <work@sunny.fr>
2016-09-28 17:11:44 +0200

    up

--- lemonde.30m.sh
+++ lemonde.30m.sh
@@ -8,18 +8,25 @@ request('http://www.lemonde.fr', function (error, response, html) {
         var block = $('[data-vr-zone="bloc_en_continu"] .pages', html).eq(0);
         var page = block.eq(0).find('ul').eq(0);
         var pageLinks = page.find('li');
-        for (var i = 0; i < pageLinks.length; i++) {
+        for (var i = 0; i < 3 ; i++) {
         	var item = pageLinks[i] ;
-            //console.log($(item).html())
         	var h = $(item).find('.heure').text();
         	var txt = $(item).find('.texte').text();
             var href = $(item).find('a').attr('href');
             var dhref = '/' + $(item).find('[data-href]').attr('data-href');
-			console.log(
-                '📰  ' + h + ' : ' + txt 
-
-                + ' | href="http://www.lemonde.fr'+(href||dhref)+'"'
-                + '  color=' + (href ? 'green' : 'yellow') );
+            var title = (href ? '📰  ' : '')  + h + ' : ' + txt
+			          console.log( title );
+        };
+        console.log("---")
+        for (var i = 0; i < pageLinks.length; i++) {
+        	var item = pageLinks[i] ;
+        	var h = $(item).find('.heure').text();
+        	var txt = $(item).find('.texte').text();
+            var href = $(item).find('a').attr('href');
+            var dhref = '/' + $(item).find('[data-href]').attr('data-href');
+            var title = (href ? '📰  ' : '')  + h + ' : ' + txt
+            var link = 'href="http://www.lemonde.fr' + ( href || dhref ) + '"'
+			          console.log( title + ' | ' + link );
         };
       }
     });
